4d3ced801332d35ab0219fe82a449d0b0994a1f6
Aimee Bojorquez <abojo079@me.com>
2019-12-10 21:42:45 -0500

    aimeezonCustomer.js

--- aimeezonCustomer.js
+++ aimeezonCustomer.js
@@ -17,13 +17,15 @@ customerQuery();
 });
 
 function customerQuery() {
+
+         console.log('Hello, welcome to Aimeezon!');
+
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     for (let i = 0; i < res.length; i++) {
       console.log(`${res[i].item_id} | ${res[i].product_name} | ${res[i].department_name} | ${res[i].price}| ${res[i].stock_quantity}`);
-     console.log('Hello, welcome to Aimeezon!');
-    }
 
+    }
 inquirer.prompt ([
     {
     type: 'input',
